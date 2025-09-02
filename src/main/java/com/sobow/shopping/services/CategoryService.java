b345ccc7da4c083f362f3412ee72354ad2fb4893
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-02 18:08:27 +0200

    Define CategoryService interface

---
+++ src/main/java/com/sobow/shopping/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.sobow.shopping.services;
+
+import com.sobow.shopping.domain.Category;
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryService {
+    
+    Category save(Category category);
+    
+    Optional<Category> findById(Long id);
+    
+    Optional<Category> findByName(String name);
+    
+    List<Category> findAll();
+    
+    void deleteById(Long id);
+    
+    boolean existsById(Long id);
+    
+    Category partialUpdateById(Category category, Long id);
+}
