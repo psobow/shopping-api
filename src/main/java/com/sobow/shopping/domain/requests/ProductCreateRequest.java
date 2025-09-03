08d76d74a2e9207ff438544dfd86042062f63cec
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-03 20:34:08 +0200

    Move ProductDto to request package

--- src/main/java/com/sobow/shopping/domain/requests/ProductCreateRequest.java
+++ src/main/java/com/sobow/shopping/domain/requests/ProductCreateRequest.java
@@ -1,4 +1,4 @@
-package com.sobow.shopping.domain.dto;
+package com.sobow.shopping.domain.requests;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
