855f1bd2022e4a8f1e4225802c3a512c4ce2c71e
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-12 23:44:57 +0200

    Define CartService interface

---
+++ src/main/java/com/sobow/shopping/services/CartService.java
@@ -0,0 +1,24 @@
+package com.sobow.shopping.services;
+
+import com.sobow.shopping.domain.Cart;
+import com.sobow.shopping.domain.CartItem;
+import java.math.BigDecimal;
+
+public interface CartService {
+    
+    Cart createCartForUser(Long userId);
+    
+    Cart findCartById(Long id);
+    
+    CartItem addCartItem(Long cartId, CartItem cartItem);
+    
+    CartItem removeCartItem(Long cartId, CartItem cartItem);
+    
+    CartItem incrementCartItemQty(Long cartId, CartItem cartItem);
+    
+    CartItem decrementCartItemQty(Long cartId, CartItem cartItem);
+    
+    void removeAllCartItems(Long cartId);
+    
+    BigDecimal getCartTotalPrice(Long cartId);
+}
