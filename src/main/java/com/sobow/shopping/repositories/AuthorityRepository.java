c69778cdaa36781632856d531cc8f755f646196a
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-17 19:26:21 +0200

    Add repository annotation

--- src/main/java/com/sobow/shopping/repositories/AuthorityRepository.java
+++ src/main/java/com/sobow/shopping/repositories/AuthorityRepository.java
@@ -3,7 +3,9 @@ package com.sobow.shopping.repositories;
 import com.sobow.shopping.domain.entities.UserAuthority;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorityRepository extends JpaRepository<UserAuthority, Long> {
     
     Set<UserAuthority> findAllByUserId(Long userId);
--- src/main/java/com/sobow/shopping/repositories/UserProfileRepository.java
+++ src/main/java/com/sobow/shopping/repositories/UserProfileRepository.java
@@ -3,7 +3,9 @@ package com.sobow.shopping.repositories;
 import com.sobow.shopping.domain.entities.UserProfile;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
     
     Optional<UserProfile> findByUserId(Long userId);
