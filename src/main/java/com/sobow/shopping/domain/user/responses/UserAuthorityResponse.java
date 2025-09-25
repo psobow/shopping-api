483aa5df1e21d25415a0ca6045b981483d832580
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-25 04:55:24 +0200

    Add @JsonValue to user authority response

--- src/main/java/com/sobow/shopping/domain/user/responses/UserAuthorityResponse.java
+++ src/main/java/com/sobow/shopping/domain/user/responses/UserAuthorityResponse.java
@@ -1,7 +1,20 @@
 package com.sobow.shopping.domain.user.responses;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public record UserAuthorityResponse(
     String value
 ) {
-
+    
+    @JsonCreator(mode = Mode.DELEGATING)
+    public UserAuthorityResponse(String value) {
+        this.value = value;
+    }
+    
+    @JsonValue
+    public String json() {
+        return value;
+    }
 }
