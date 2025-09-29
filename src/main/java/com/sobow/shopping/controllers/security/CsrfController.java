574885676c88c8efdc09d64d6252e550638d868e
Patryk Sobow <patryk.sobow95@gmail.com>
2025-09-29 02:56:49 +0200

    Add docs for CsrfController

--- src/main/java/com/sobow/shopping/controllers/security/CsrfController.java
+++ src/main/java/com/sobow/shopping/controllers/security/CsrfController.java
@@ -1,5 +1,9 @@
 package com.sobow.shopping.controllers.security;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${api.prefix}")
+@Tag(
+    name = "CSRF Controller",
+    description = "API to generate CSRF token"
+)
 public class CsrfController {
     
+    @Operation(
+        summary = "Get CSRF token"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "CSRF token generated")
+    })
     @GetMapping("/csrf")
     public CsrfToken csrf(CsrfToken token) {
         return token;
